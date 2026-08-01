8407fb11a2c41edc9e994edf8b3bc28a1f062bdf
Waqar Ahmed Tunio <57320785+wikitunio@users.noreply.github.com>
2026-08-01 19:22:06 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,35 +26,6 @@ from config import DEFAULT_DARK_MODE
 
 if "dark_mode" not in st.session_state:
     st.session_state.dark_mode = DEFAULT_DARK_MODE
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["DASHBOARD_PASSWORD"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        st.markdown("<h2 style='text-align: center;'>🔒 Secure Login</h2>", unsafe_allow_html=True)
-        st.text_input("Enter Dashboard Password", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        st.markdown("<h2 style='text-align: center;'>🔒 Secure Login</h2>", unsafe_allow_html=True)
-        st.text_input("Enter Dashboard Password", type="password", on_change=password_entered, key="password")
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        return True
-
-def authenticate_gmail():
-    creds = Credentials(
-        token=None,
-        refresh_token=st.secrets["GMAIL_REFRESH_TOKEN"],
-        token_uri="https://oauth2.googleapis.com/token",
-        client_id=st.secrets["GMAIL_CLIENT_ID"],
-        client_secret=st.secrets["GMAIL_CLIENT_SECRET"]
-    )
-    return build('gmail', 'v1', credentials=creds)
 
 @st.cache_data(ttl=3600)
 def fetch_salary_data():
